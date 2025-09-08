97b43f50285a523785129cd29ee7318947f04101
TheBestlegend999 <125496238+TheBestlegend999@users.noreply.github.com>
2025-09-08 15:33:48 +0200

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,26 @@
+
+# Assign variables with identifiers that best describe
+# the email properties such as title, content, sender, etc.
+# Assign appropriate values to these variables
+# Display these variables in the console using the print() function
+
+
+
+
+
+
+title = "First email"
+content = "Hello World"
+sender = "Johny"
+receiver = "AIGuy"
+emailDate = "08.09.2025"
+numAttachments = 1
+attachmentName = "AIConqueringTheWorld.pdf"
+
+print("Email Title: " + title)
+print("Email Content: ", content)
+print("Sender: ", sender)
+print("Receiver: ", receiver)
+print("EmailDate: ", emailDate)
+print("Number of Attachments: ", numAttachments)
+print("Attachment nr 1: ", attachmentName)
