284ab8cea9d11525aa5d1bd02306aa5ab1bf576e
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-09-09 19:19:30 +0200

    Add files via upload

---
+++ task02.py
@@ -0,0 +1,12 @@
+# 1. Calculate the average of the following numbers: 1, 2, 3 and 4
+# 2. Assign the calculated value to the variable averageNum
+#    while calculating remember to use parentheses
+#    for correct order of operations
+#    Print the average of the numbers 1, 2, 3, 4 to the console
+# 3. Also show the type of this average using the function: type
+
+
+
+averageNum = (1 + 2 + 3 + 4) / 4
+print("Average of the numbers:", averageNum)
+print("type" , type(averageNum))
