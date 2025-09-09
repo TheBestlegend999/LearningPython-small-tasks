13129a8ca9e8cd97c73650a2ab86d7912d6f01ff
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-09-09 19:32:27 +0200

    Add files via upload

---
+++ task03.py
@@ -0,0 +1,11 @@
+# Calculate BMI
+
+# 18.5 < BMI < 24.9
+
+# bmi = weight / (height * height)
+
+# Calculate bmi for weight 70 and height 1.75, then display in console
+
+
+BMI = 70 / (1.75 * 1.75)
+print ("BMI Index :" , BMI)
