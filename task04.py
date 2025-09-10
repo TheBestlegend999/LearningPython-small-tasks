22b99cef8237f18f451b67ef8632e259c5b428d8
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-09-10 06:02:27 +0200

    Add files via upload

---
+++ task04.py
@@ -0,0 +1,19 @@
+# 1. Use the built-in input() function in Python 
+# to get information from the user via console. 
+# Ask the user to provide their first name, last name, and city 
+# Save this information in the variables name, surname, and city
+# 2. Display in the console a summary text with the collected data:
+# "Your name is Ania Kowalska and you live in: Krk"
+
+
+
+print("Enter your name:")
+name = input()
+
+print ("Enter your surname:")
+surname = input()
+
+print ("Enter your city:")
+city = input()
+
+print("Your name is " + name + " " + surname + "  and you live in " + city)
