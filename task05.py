def4719de186943c09065504da0e33c0e9609a01
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-09-11 20:21:51 +0200

    Add files via upload

---
+++ task05.py
@@ -0,0 +1,25 @@
+# 1. Write a program that checks if the number 10 in the variable num is:
+#    - positive
+#    - negative
+#    - equal to zero
+#
+# 2. Display in the console what kind of number it is.
+#
+# 3. Remember to use the if conditional statement (and indentation) e.g.:
+# if num > 4:
+#     print(num, "is greater than 4")
+#
+# if someNumber == 1:
+#     print("the number is equal to one")
+
+
+num = 10
+
+if num > 0:
+    print("positive")
+
+if num < 0:
+    print("negative")
+
+if num == 0:
+    print("zero")
