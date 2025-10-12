32ceb196fd11d8009eed5997897e734b11615271
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-10-12 13:09:38 +0200

    Add files via upload

---
+++ tasks/14 bank_account_operations.py
@@ -0,0 +1,11 @@
+balance = 1000
+print("Stan Konta: ",balance)
+balance += 7000
+print("Stan konta: ", balance, "(Wypłata) +7000")
+balance -= 2000
+print("Stan konta: ", balance, "(Koszty mieszkania) -2000")
+balance *= 3
+print("Stan konta: ", balance, "(Błąd Banku) +12000") 
+balance -= 4000
+print("Stan konta: ", balance, "(Zakup komputera) -4000")
+
