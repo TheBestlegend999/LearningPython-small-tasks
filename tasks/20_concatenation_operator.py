49822e5fcebe161efda3ea9e8f6aa6544b6ff13f
Jaggy <125496238+TheBestlegend999@users.noreply.github.com>
2025-10-13 17:09:32 +0200

    Add files via upload

---
+++ tasks/20_concatenation_operator.py
@@ -0,0 +1,19 @@
+firstName = "Adam "
+
+lastName = "Silverhand"
+fullName = firstName  +  lastName
+print(fullName)
+
+listOne = [1,2,3]
+listTwo = [4,5,6]
+combinedList = listOne + listTwo
+print(combinedList)
+if len(combinedList) > 4:
+    print("Lista jest dluga")
+
+greeting = " Hello "  + fullName
+
+
+if "Hello" in greeting:
+    print(greeting)
+
